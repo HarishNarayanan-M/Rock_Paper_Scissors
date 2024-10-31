6775a3791624cd757516e2c89774f77ecfa95e66
HarishNarayanan-M <140610673+HarishNarayanan-M@users.noreply.github.com>
2024-10-31 14:36:32 +0530

    Write a function to get computer choice & print it

--- script.js
+++ script.js
@@ -1,1 +1,15 @@
 let randomNumber=Math.floor(Math.random()*100);
+
+function getComputerChoice(){
+    if(randomNumber>=0&& randomNumber<=33){
+        console.log("Rock");
+    }
+    if(randomNumber>33&& randomNumber<=66){
+        console.log("Paper");
+    }
+    if(randomNumber>66&& randomNumber<=100){
+        console.log("Scissor");
+    }
+}
+
+console.log(getComputerChoice());
