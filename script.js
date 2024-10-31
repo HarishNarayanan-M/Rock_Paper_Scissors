19b7392daac07d3123c535107a35623dcea29087
HarishNarayanan-M <140610673+HarishNarayanan-M@users.noreply.github.com>
2024-10-31 14:56:33 +0530

    Create a function to get human choice & print it

--- script.js
+++ script.js
@@ -4,12 +4,32 @@ function getComputerChoice(){
     if(randomNumber>=0&& randomNumber<=33){
         console.log("Rock");
     }
-    if(randomNumber>33&& randomNumber<=66){
+    else if(randomNumber>33&& randomNumber<=66){
         console.log("Paper");
     }
-    if(randomNumber>66&& randomNumber<=100){
+    else{
         console.log("Scissor");
     }
 }
 
 console.log(getComputerChoice());
+
+function getHumanChoice(){
+   let choice= prompt("Enter you choice 1.Rock 2.Paper 3.Scissor").toLowerCase();
+
+   if(choice=="rock"){
+    console.log("Rock")
+   }
+   else if(choice=="paper"){
+    console.log("Paper");
+   }
+   else if(choice=="scissor"){
+    console.log("scissor");
+   }
+   else{
+    console.log("Invalid choice");
+   }
+
+}
+
+getHumanChoice();
